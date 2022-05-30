b1272217e32e29170fda9642f1e1f007f89b6a72
Takekazu Omi <takekazuomi@gmail.com>
2022-05-30 01:52:05 +0000

    !!!

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello world with GitHub Action !! "+build)
+		io.WriteString(w, "Hello world with GitHub Action !!! "+build)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
